067d52cd5f1f30634b7f332b33d0ee181594508f
Chen Bin <chenbin.sh@gmail.com>
2016-12-13 16:00:57 +1100

    clean code and README

--- js-comint.el
+++ js-comint.el
@@ -7,7 +7,7 @@
 ;;; Author: Paul Huff <paul.huff@gmail.com>, Stefano Mazzucco <MY FIRST NAME - AT - CURSO - DOT - RE>
 ;;; Maintainer: Chen Bin <chenbin.sh AT gmail DOT com>
 ;;; Created: 15 Feb 2014
-;;; Version: 0.0.5
+;;; Version: 1.0.0
 ;;; URL: https://github.com/redguardtoo/js-comint
 ;;; Package-Requires: ()
 ;;; Keywords: javascript, node, inferior-mode, convenience
@@ -60,6 +60,8 @@
 
 ;;  Do: `M-x run-js'
 ;;  Away you go.
+;;  `node_modules' is *automatically* searched and appended into environment
+;;  variable `NODE_PATH'. So 3rd party javascript is usable out of box.
 
 ;;  If you have nvm, you can select the versions of node.js installed and run
 ;;  them.  This is done thanks to nvm.el.
@@ -68,8 +70,19 @@
 ;;  The first time you start the JS interpreter with run-js, you will be asked
 ;;  to select a version of node.js
 ;;  If you want to change version of node js, run `js-select-node-version'
-
-;;  You can add  the following couple of lines to your .emacs to take advantage of
+;;
+;;  `js-clear' clears the content of REPL.
+;;
+;; You may get cleaner output by following setup (highly recommended):
+;;
+;;   (defun inferior-js-mode-hook-setup ()
+;;     (add-hook 'comint-output-filter-functions 'js-comint-process-output))
+;;   (add-hook 'inferior-js-mode-hook 'inferior-js-mode-hook-setup t)
+;;
+;;  `js-comint-process-output' uses `js-comint-drop-regexp' which could be
+;;  customized by users.
+;;
+;;  You can add the following lines to your .emacs to take advantage of
 ;;  cool keybindings for sending things to the javascript interpreter inside
 ;;  of Steve Yegge's most excellent js2-mode.
 ;;
@@ -154,8 +167,7 @@ Return a string representing the node version."
            "/node"))))
 
 (defun js--is-nodejs ()
-  (string= "node"
-           (substring-no-properties inferior-js-program-command -4 nil)))
+  (string-match-p "node$" inferior-js-program-command))
 
 (defun js--guess-load-file-cmd (filename)
   (let ((cmd (concat "require(\"" filename "\")\n")))
