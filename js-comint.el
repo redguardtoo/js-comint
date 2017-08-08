eb4744122724b24e492c2171fff438e3ee2045a8
Chen Bin <chenbin.sh@gmail.com>
2017-08-08 22:27:17 +1000

    upgrade license

--- js-comint.el
+++ js-comint.el
@@ -8,36 +8,26 @@
 ;;; Maintainer: Chen Bin <chenbin.sh AT gmail DOT com>
 ;;; Created: 15 Feb 2014
 ;;; Version: 1.1.0
-;;; URL: https://github.com/redguardtoo/js-comint
+;;; URL: https://github.com/js-comint/js-comint
 ;;; Package-Requires: ()
 ;;; Keywords: javascript, node, inferior-mode, convenience
 
-;; This file is NOT part of GNU Emacs.
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
 
-;;; License:
-
-;; This program is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License as
-;; published by the Free Software Foundation; either version 2, or
-;; at your option any later version.
-
-;; js-comint.el is distributed in the hope that it will be useful, but
-;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-;; General Public License for more details.
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs; see the file COPYING, or type `C-h C-c'. If
-;; not, write to the Free Software Foundation at this address:
-
-;;   Free Software Foundation
-;;   51 Franklin Street, Fifth Floor
-;;   Boston, MA 02110-1301
-;;   USA
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
-;; js-comint.el is a comint mode for Emacs which allows you to run a
+;; This program is a comint mode for Emacs which allows you to run a
 ;; compatible javascript repl like Node.js/Spidermonkey/Rhino inside Emacs.
 ;; It also defines a few functions for sending javascript input to it
 ;; quickly.
