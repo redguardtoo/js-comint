dd9c0ed2c8f47e0c4e7394b071cfc04527ae8cc4
Josh Bax <joshbax189@gmail.com>
2024-10-22 12:37:46 -0700

    Update github action to use eask
    Remove unused script files

--- install-deps.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -xeu
-
-apt-get update
-apt-get install -y nodejs
-apt-get clean
--- runtests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -xeu
-
-cd "$(dirname $0)"
-emacs -Q --batch \
-      -l ert \
-      -l js-comint.el \
-      -l test-js-comint.el \
-      -f ert-run-tests-batch-and-exit
